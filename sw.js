d3c34662c98b65f9d66e923814901b2401fcac61
PetSitterLili <petsitterlili2025@gmail.com>
2026-05-08 04:39:37 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'lili-pet-v34';
+const assets = ['./', './index.html', './20260508_043325_0000.png', './cat_orange.png', './ig.png', './line.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
