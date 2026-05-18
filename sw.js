18250532dd10a2bfb18a977377ef002f12ac3002
PetSitterLili <petsitterlili2025@gmail.com>
2026-05-18 16:18:45 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lili-pet-v34.2';
+const CACHE_NAME = 'lili-pet-v34.5';
 const assets = ['./', './index.html', './20260508_043325_0000.png', './cat_orange.png', './ig.png', './line.png'];
 
 self.addEventListener('install', (e) => {
